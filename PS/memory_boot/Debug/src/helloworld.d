8bf1b17f1b3e11c0936eca833c45eeea3884df72
Seradory <aliserdar.yurtcan@hotmail.com>
2023-12-07 18:29:07 +0300

    kod eklendi

---
+++ PS/memory_boot/Debug/src/helloworld.d
@@ -0,0 +1,18 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_printf.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_printf.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/bspconfig.h:
---
+++ PS/memory_boot/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_cache.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/mb_interface.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_assert.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_exception.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h \
+ ../src/platform_config.h
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_cache.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/mb_interface.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_assert.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_exception.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/MEMORY_WORK/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h:
+
+../src/platform_config.h:
